7841e5beea221386aab641a9fac14518005e054e
arjunjanamatti <arjunjanamatti@gmail.com>
2022-11-22 22:07:03 +0530

    Problem and initial solution

---
+++ solveMeFirst.py
@@ -0,0 +1,12 @@
+############ Problem
+# Complete the function solveMeFirst to compute the sum of two integers
+
+
+def solveMeFirst(a,b):
+    return a + b
+
+if __name__ == '__main__':
+    num1 = int(input())
+    num2 = int(input())
+    result = solveMeFirst(num1, num2)
+    print(result)
