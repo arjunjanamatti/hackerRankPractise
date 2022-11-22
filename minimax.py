ddd3c2254e9f93b4658b26d4d28ff86ea561f5e7
arjunjanamatti <arjunjanamatti@gmail.com>
2022-11-22 21:55:30 +0530

    time complexity simple explanation

--- minimax.py
+++ minimax.py
@@ -5,3 +5,9 @@
 # For example arr = [1,3,5,7,9], minimum sum is 1 + 3 + 5 + 7 = 16
 # and maximum sum is 3 + 5 + 7 + 9 = 24
 #
+
+# Time complexity
+# O(1): order of 1 or constant time, like 2 + 3
+# O(log n): means splitting the inputs each step, like deviding it by 2
+# O(n): is order over n, like single for loop
+# O(n^2): order to power n**2, double for loop over "n" size
