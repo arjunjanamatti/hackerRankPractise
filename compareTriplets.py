60f3ef8af4ce7fbff2d08c76e7e8a502e92e7821
arjunjanamatti <arjunjanamatti@gmail.com>
2022-11-23 22:30:47 +0530

    completed problem statement

---
+++ compareTriplets.py
@@ -0,0 +1,11 @@
+############ Problem
+# Alice and Bob each created one problem for HackerRank. A reviewer rates the two
+# challenges, awarding points on a scale from 1 to 100 for three categories:
+# problem clarity, originality and difficulty.
+# We define the rating for Alice's challenge to be the triplet
+# a = (a[0], a[1], a[2]) and rating for Bob's challenge to be the triplet
+# b = (b[0], b[1], b[2]).
+# Compare a[0] with b[0], and continuing for all 3.
+#   - If a[i] > b[i], then Alice is awarded 1 point
+#   - If a[i] < b[i], then Bob is awarded 1 point
+#   - If a[i] = b[i], then neither person receives a point
