60be3ead991295507f9a4e1495bf80f23c518db4
arjunjanamatti <arjunjanamatti@gmail.com>
2022-11-24 21:56:36 +0530

    completed problem description

---
+++ verBigSum.py
@@ -0,0 +1,6 @@
+###########Problem
+# Calculate and print the sum of the elements in an array, keeping in mind
+# that some of those integers may be quite large.
+# Function Description
+# Complete the VeryBigSum function in the editor below, it must return the sum
+# of all array elements.
