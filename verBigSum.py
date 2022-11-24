adb1b9cbb9083b93f7d8668a95545e9b373c45aa
arjunjanamatti <arjunjanamatti@gmail.com>
2022-11-24 22:00:49 +0530

    final solution

--- verBigSum.py
+++ verBigSum.py
@@ -4,3 +4,15 @@
 # Function Description
 # Complete the VeryBigSum function in the editor below, it must return the sum
 # of all array elements.
+
+
+def aVeryBigSum(ar):
+    return sum(ar)
+
+
+if __name__ == "__main__":
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
