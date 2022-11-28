1868479f17c4157f47b84d52adfe9c1d9672e02b
arjunjanamatti <arjunjanamatti@gmail.com>
2022-11-28 18:21:02 +0530

    completed diagonalDifference problem statement

--- diagonalDifference.py
+++ diagonalDifference.py
@@ -4,3 +4,17 @@
 # For example, the square matrix is [1,2,3; 4,5,6; 9,8,9]
 # The left to right diagonal = 1 + 5 + 9 = 15. The right to left diagonal
 #  = 3 + 5+ 9 = 17. Their absolute difference is |15 - 17| = 2
+
+def diagonalDifference(arr):
+    pass
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+    print(arr)
